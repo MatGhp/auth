bff4bb0388fab91e990faaa25708fbd1a6142fe1
MatGhp <mat.ghp@gmail.com>
2018-03-07 14:46:10 +0100

    add JWT feature to Accountcontroller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+using Auth.Extensions;
+using Auth.Core.Models;
+using Auth.Core;
+using Auth.Persistence.Services;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("CorsPolicy")]
+    public class AccountController : Controller
+    {
+        private readonly IUsersService _usersService;
+        private readonly ITokenStoreService _tokenStoreService;
+        private readonly IUnitOfWork _uow;
+
+        public AccountController(
+            IUsersService usersService,
+            ITokenStoreService tokenStoreService,
+            IUnitOfWork uow)
+        {
+            _usersService = usersService;
+            _usersService.CheckArgumentIsNull(nameof(usersService));
+
+            _tokenStoreService = tokenStoreService;
+            _tokenStoreService.CheckArgumentIsNull(nameof(_tokenStoreService));
+
+            _uow = uow;
+            _uow.CheckArgumentIsNull(nameof(_uow));
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody]  User loginUser)
+        {
+            if (loginUser == null)
+            {
+                return BadRequest("user is not set.");
+            }
+
+            var user = await _usersService.FindUserAsync(loginUser.Username, loginUser.Password);
+            if (user == null || !user.IsActive)
+            {
+                return Unauthorized();
+            }
+
+            var (accessToken, refreshToken) = await _tokenStoreService.CreateJwtTokens(user, refreshTokenSource: null);
+            return Ok(new { access_token = accessToken, refresh_token = refreshToken });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RefreshToken([FromBody]JToken jsonBody)
+        {
+            var refreshToken = jsonBody.Value<string>("refreshToken");
+            if (string.IsNullOrWhiteSpace(refreshToken))
+            {
+                return BadRequest("refreshToken is not set.");
+            }
+
+            var token = await _tokenStoreService.FindTokenAsync(refreshToken);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            var (accessToken, newRefreshToken) = await _tokenStoreService.CreateJwtTokens(token.User, refreshToken);
+            return Ok(new { access_token = accessToken, refresh_token = newRefreshToken });
+        }
+
+        [AllowAnonymous]
+        [HttpGet("[action]"), HttpPost("[action]")]
+        public async Task<bool> Logout([FromBody]JToken jsonBody)
+        {
+            var claimsIdentity = this.User.Identity as ClaimsIdentity;
+            var userIdValue = claimsIdentity.FindFirst(ClaimTypes.UserData)?.Value;
+            var refreshToken = jsonBody.Value<string>("refreshToken");
+
+            // The Jwt implementation does not support "revoke OAuth token" (logout) by design.
+            // Delete the user's tokens from the database (revoke its bearer token)
+            await _tokenStoreService.RevokeUserBearerTokensAsync(userIdValue, refreshToken);
+            await _uow.SaveChangesAsync();
+
+            return true;
+        }
+
+        [HttpGet("[action]"), HttpPost("[action]")]
+        public bool IsAuthenticated()
+        {
+            return User.Identity.IsAuthenticated;
+        }
+
+        [HttpGet("[action]"), HttpPost("[action]")]
+        public IActionResult GetUserInfo()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            return Json(new { Username = claimsIdentity.Name });
+        }
+    }
+}
